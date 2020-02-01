a029ccbe7f53fd39db685547efa13c2d4146e9de
ALEX SKRIVSETH <alexskrivseth@gmail.com>
2020-02-01 15:42:41 -0800

    cleaned up some of the code and added a few docs

--- kiln_app/kiln_app/settings/settings.py
+++ kiln_app/kiln_app/settings/settings.py
@@ -130,15 +130,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+# this is where "collectstatic" will collect the staticfiles
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# I am still unclear on how this works
+# need to study
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
-#
+
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
