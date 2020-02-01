8680e21e6339772308e4b47dd9a2a5b87801b2b3
ALEX SKRIVSETH <alexskrivseth@gmail.com>
2020-01-31 19:18:56 -0800

    take 3 on this

--- kiln_app/kiln_app/settings/settings.py
+++ kiln_app/kiln_app/settings/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
+
     'django.contrib.staticfiles',
     #thirdparty
     'rest_framework',
@@ -130,10 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 #
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
