dbaea83f93ee8ad943166730226030ccf5a4f74c
ALEX SKRIVSETH <alexskrivseth@gmail.com>
2020-01-31 18:57:53 -0800

    changed the settings file again

--- kiln_app/kiln_app/settings/settings.py
+++ kiln_app/kiln_app/settings/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
+    
     'django.contrib.staticfiles',
     #thirdparty
     'rest_framework',
