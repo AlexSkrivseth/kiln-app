0153956463d5afca1d062f55a48b45b5b0ebbf68
ALEX SKRIVSETH <alexskrivseth@gmail.com>
2020-03-24 08:25:57 -0700

    Made some trivial changes like getting rid of spaces

--- kiln_app/kiln_app/settings/production.py
+++ kiln_app/kiln_app/settings/production.py
@@ -5,9 +5,10 @@ from .settings import *
 
 ALLOWED_HOSTS = ['kiln-app.herokuapp.com', 'localhost', '127.0.0.1']
 
-# remeber to set this enviromental variable in you production server
+# remember to set this enviromental variable in you production server
 SECRET_KEY = os.getenv(
     'DJANGO_SECRET_KEY',
     '^8ai-6gb)yyg19uangdahsi8a%c=)mb0yler7%0jlh1mz!^gnago;91_')
 
+# overwriting the basic settings
 DEBUG = False
