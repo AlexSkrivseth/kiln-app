84cd3cd9d89603ef2b48c8374c7c7d153e4e7837
ALEX SKRIVSETH <alexskrivseth@gmail.com>
2020-05-21 09:50:15 -0700

    made a change to the template to include some notes about the process

--- kiln_app/kiln_app/settings/dev.py
+++ kiln_app/kiln_app/settings/dev.py
@@ -1,1 +1,13 @@
 from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dmtltpue5kc80',
+        'USER': 'vaplgmvzyhxcne',
+        'PASSWORD': 'c5b644607d8acde39a3f000ee438965ed994a71da2d371fe851c5912462c61f5',
+        'HOST': 'ec2-50-19-254-63.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'URI': 'postgres://vaplgmvzyhxcne:c5b644607d8acde39a3f000ee438965ed994a71da2d371fe851c5912462c61f5@ec2-50-19-254-63.compute-1.amazonaws.com:5432/dmtltpue5kc80'
+    }
+}
