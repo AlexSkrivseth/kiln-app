2cfe03e61321160a558eaccd02631ceed14adb69
Alex Skrivseth <alex@vikingcode.io>
2020-06-01 08:57:53 -0700

    commented out Token authentication

--- kiln_app/kiln_app/settings/settings.py
+++ kiln_app/kiln_app/settings/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         ### YOUR CODE HERE
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
         #'api.authentication.APIClientAuthentication',
         ),
 
