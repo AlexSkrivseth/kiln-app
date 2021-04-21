19b0dfc18f94d1ac1f6ecaf2461bedb399dc6012
ALEX SKRIVSETH <alexskrivseth@ALEXs-MBP-2.lan>
2021-04-20 21:24:42 -0700

    Made some simplifications and started work on some simpler charts

--- kiln_app/dashboard/urls.py
+++ kiln_app/dashboard/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.main),
     path('dev', views.dev),
     path('kiln/<int:kiln_id>/', views.kiln),
+    path('main_dev', views.main_dev),
 
 
 ]
--- kiln_app/dashboard/views.py
+++ kiln_app/dashboard/views.py
@@ -119,3 +119,78 @@ def dev(request):
 
 
     return render(request, 'dashboard/main.html', context=context)
+
+
+def main_dev(request):
+    # hit the darksky api and get the current conditions
+    r = requests.get('https://api.darksky.net/forecast/be0d42628d574bdbab49edeaa13d3759/48.721625,-116.303509')
+    data = r.json()
+    current_conditions = {
+    'outside_temp': data['currently']['temperature'],
+    'outside_humid': 100 * data['currently']['humidity'],
+    'atmospheric_pressure':data['currently']['pressure']
+    }
+
+    try:
+        # gather latest reading from each kiln load
+        kiln1_readings = Reading.objects.filter(load__kiln=1)
+        reading1 = kiln1_readings.latest('timestamp')
+        kiln2_readings = Reading.objects.filter(load__kiln=2)
+        reading2 = kiln2_readings.latest('timestamp')
+
+        loads = Load.objects.filter(active=True)
+        load1 = loads.get(kiln=1)
+        load2 = loads.get(kiln=2)
+        daynum1 = (datetime.now(timezone.utc) - load1.startdate).days
+        daynum2 = (datetime.now(timezone.utc) - load2.startdate).days
+        # do some calculations with the temperature and the humidity
+        # to get the absolute humidity in the kiln
+        # absolute_humidity is in the api.utils file
+        kiln1ah = absolute_humidity(t=reading1.temperature, rh=reading1.humidity)
+        kiln2ah = absolute_humidity(t=reading2.temperature, rh=reading2.humidity)
+
+        # Do some logic here to see how the kilns are trending.
+        # eg  trend = Reading.NOW - READING.24hoursPAST
+        #trend = {'kiln1':{'temp': }}
+                                        # could make this dynamic
+        old_timestamp1 = reading1.timestamp - timedelta(days=1)
+        old_timestamp2 = reading2.timestamp - timedelta(days=1)
+
+        old_reading1_set = kiln1_readings.filter(timestamp__lt=old_timestamp1)
+
+        old_reading2_set = kiln2_readings.filter(timestamp__lt=old_timestamp2)
+        old_reading1 = old_reading1_set.latest('timestamp')
+        old_reading2 = old_reading2_set.latest('timestamp')
+
+        old_kiln1ah = absolute_humidity(t=old_reading1.temperature, rh=old_reading1.humidity)
+        old_kiln2ah = absolute_humidity(t=old_reading2.temperature, rh=old_reading2.humidity)
+
+        trend = {
+            'kiln1':{
+                'temperature': round(reading1.temperature - old_reading1.temperature, 2),
+                'humidity': round(reading1.humidity - old_reading1.humidity, 2),
+                'ahumidity': round(kiln1ah - old_kiln1ah, 2),
+            },
+            'kiln2':{
+                'temperature': round(reading2.temperature - old_reading2.temperature, 2),
+                'humidity': round(reading2.humidity - old_reading2.humidity, 2),
+                'ahumidity': round(kiln2ah - old_kiln2ah, 2),
+            },
+
+        }
+
+
+        # pass the readings as context
+        context = {
+                    'reading1': reading1,
+                    'reading2': reading2,
+                    'current_conditions': current_conditions,
+                    'ah1': kiln1ah,
+                    'ah2': kiln2ah,
+                    'trend': trend,
+                    'daynum1': daynum1,
+                    'daynum2': daynum2,
+          }
+        return render(request, 'dashboard/main_dev.html', context=context)
+    except Exception as e:
+        return HttpResponse("Fail {}".format(e))
